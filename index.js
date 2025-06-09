55dad2706add6d1e96d2873e6c1c9874770d6cb6
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-06-09 23:54:10 +0100

    Add initial post method processing

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 import http from 'http';
 
-const job = {
-    "title": "Software Engineer",
-    "company": "Tech Guys",
-    "source": "Indeed",
-    "stage": "Applied"
-}
+const jobs = [
+    {
+        "title": "Software Engineer",
+        "company": "Tech Guys",
+        "source": "Indeed",
+        "stage": "Applied"
+    },
+    {
+        "title": "Software Developer",
+        "company": "Computer People",
+        "source": "Recruiter C",
+        "stage": "Interview 1"
+    }
+]
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET'){
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.writeHead(200, { 'Content-Type': 'application/json' });
         console.log(res.getHeaders());
-        res.end(JSON.stringify(job));
+        res.end(JSON.stringify(jobs));
+    }
+    else if (req.method === 'POST'){
+        console.log('Received a post request');
+        let body = '';
+        
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            console.log(body);
+            let title = body.split("=")[1];
+            title = title.replace("+", " ");
+            console.log(title);
+            const newJob = {
+                "title": title,
+                "company": "Placeholder Company",
+                "source": "Placeholder Source",
+                "stage": "Placeholder Stage"
+            };
+
+            jobs.push(newJob);
+
+            res.statusCode = 201;
+            res.write(JSON.stringify(newJob));
+            res.end();
+        });
+
     }
 });
 
